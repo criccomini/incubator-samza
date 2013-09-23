6450218aa607ab4a74b6e14f1a04a14ce6e6b9b4
Chris Riccomini <criccomini.aux@gmail.com>
2013-09-23 13:00:05 -0700

    adding wiring in default chooser.

---
+++ samza-core/src/main/scala/org/apache/samza/config/DefaultChooserConfig.scala
@@ -0,0 +1,30 @@
+package org.apache.samza.config
+
+import org.apache.samza.system.SystemStream
+import TaskConfig._
+
+object DefaultChooserConfig {
+  val BOOTSTRAP_PREFIX = "task.chooser.bootstrap.%s.%s"
+  val PRIORITY_PREFIX = "task.chooser.prioriites.%s.%s"
+  val BATCH_SIZE = "task.chooser.batch.size"
+
+  implicit def Config2DefaultChooser(config: Config) = new DefaultChooserConfig(config)
+}
+
+class DefaultChooserConfig(config: Config) extends ScalaMapConfig(config) {
+  import DefaultChooserConfig._
+
+  def getChooserBatchSize = getOption(BATCH_SIZE)
+
+  def getBootstrapStreams = config
+    .getInputStreams
+    .map(systemStream => (systemStream, getOrElse(BOOTSTRAP_PREFIX format (systemStream.getSystem, systemStream.getStream), "false").equals("true")))
+    .filter(_._2.equals("true"))
+    .map(_._1)
+
+  def getPriorityStreams = config
+    .getInputStreams
+    .map(systemStream => (systemStream, getOrElse(PRIORITY_PREFIX format (systemStream.getSystem, systemStream.getStream), "-1").toInt))
+    .filter(_._2 >= 0)
+    .toMap
+}
--- samza-core/src/main/scala/org/apache/samza/system/DefaultChooser.scala
+++ samza-core/src/main/scala/org/apache/samza/system/DefaultChooser.scala
@@ -1,12 +1,33 @@
 package org.apache.samza.system
 
 import org.apache.samza.config.Config
+import org.apache.samza.config.DefaultChooserConfig._
+import org.apache.samza.config.TaskConfig._
 
+// TODO javadocs for DefaultChooser
 class DefaultChooserFactory extends MessageChooserFactory {
   def getChooser(config: Config): MessageChooser = {
+    val batchSize = config
+      .getChooserBatchSize
+      .getOrElse("100")
+      .toInt
+    val prioritizedBootstrapStreams = config
+      .getBootstrapStreams
+      .map((_, Int.MaxValue))
+      .toMap
+    val defaultPrioritizedStreams = config
+      .getInputStreams
+      .map((_, 0))
+      .toMap
+    val prioritizedStreams = defaultPrioritizedStreams ++ config.getPriorityStreams ++ prioritizedBootstrapStreams
     val base = new RoundRobinChooser
-    val batching = new BatchingChooser(base)
-    val priority = new TieredPriorityChooser(Map(), Map())
+    val batching = new BatchingChooser(base, batchSize)
+    val prioritizedChoosers = prioritizedStreams
+      .values
+      .toSet
+      .map((_: Int, new BatchingChooser(base, batchSize).asInstanceOf[MessageChooser]))
+      .toMap
+    val priority = new TieredPriorityChooser(prioritizedStreams, prioritizedChoosers)
     val bootstrapping = new BootstrappingChooser(Map(), priority)
     bootstrapping
   }
