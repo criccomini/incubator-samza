6f25d4deadb79df343a128b9a7ee6f58fdc23462
Chris Riccomini <criccomi@criccomi-mn.linkedin.biz>
2014-10-22 15:03:11 -0700

    update docs. remove job servlet test since it's testing in samza container.

--- samza-shell/src/main/bash/run-am.sh
+++ samza-shell/src/main/bash/run-am.sh
@@ -20,6 +20,6 @@
 [[ $JAVA_OPTS != *-server* ]] && JAVA_OPTS="$JAVA_OPTS -server"
 
 # Set container ID and name system properties for use in Log4J
-JAVA_OPTS="$JAVA_OPTS -Dsamza.container.id=-1 -Dsamza.container.name=samza-application-master"
+JAVA_OPTS="$JAVA_OPTS -Dsamza.container.name=samza-application-master"
 
 exec $(dirname $0)/run-class.sh org.apache.samza.job.yarn.SamzaAppMaster $@
