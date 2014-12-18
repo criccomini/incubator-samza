c999cfb7f87130ff41a6ebb464872f12976d92fb
Chris Riccomini <criccomi@criccomi-mn.linkedin.biz>
2014-12-18 14:50:56 -0800

    everything installs except samza job now.

--- samza-test/src/main/python/deployment.py
+++ samza-test/src/main/python/deployment.py
@@ -9,144 +9,104 @@ import kafka_deployer as kd
 from zopkio.runtime import get_active_config as c
 
 logger = logging.getLogger(__name__)
+deployers = None
 samza_deployer = None
-kafka_deployer = None
-ssh_deployer = None
 
 def _download_packages():
-  for url_key in ["url_hadoop", "url_kafka", "url_zookeeper"]:
-    logger.debug("Getting download URL for: {0}".format(url_key))
+  for url_key in ['url_hadoop', 'url_kafka', 'url_zookeeper']:
+    logger.debug('Getting download URL for: {0}'.format(url_key))
     url = c(url_key)
     filename = os.path.basename(url)
     if os.path.exists(filename):
-      logger.debug("Using cached file: {0}".format(filename))
+      logger.debug('Using cached file: {0}'.format(filename))
     else:
-      logger.info("Downloading: {0}".format(url))
+      logger.info('Downloading: {0}'.format(url))
       urllib.urlretrieve(url, filename)
 
-def _install_yarn(deployer, unique_id):
-  logger.debug("Installing YARN.")
-  deployer.install(unique_id, {
-    'hostname': c("yarn_rm_hostname"),
-    'install_path': os.path.join(c("remote_install_path"), c("yarn_install_path")),
-    'executable': c("yarn_executable"),
+def _new_deployer(config_prefix, name=None):
+  deployer_name = config_prefix if name == None else name
+  return adhoc_deployer.SSHDeployer(deployer_name, {
+    'install_path': os.path.join(c('remote_install_path'), c(config_prefix + '_install_path')),
+    'executable': c(config_prefix + '_executable'),
+    'post_install_cmds': c(config_prefix + '_post_install_cmds', []),
+    'start_command': c(config_prefix + '_start_cmd'),
+    'stop_command': c(config_prefix + '_stop_cmd'),
+    'extract': True,
+    'sync': True,
   })
-
-def _start_yarn_rm(deployer, unique_id):
-  _start_yarn(deployer, unique_id, "rm")
-
-def _start_yarn_nm(deployer, unique_id):
-  _start_yarn(deployer, unique_id, "nm")
-
-def _start_yarn(deployer, unique_id, type="nm"):
-  logger.debug("Starting YARN.")
-  deployer.start(unique_id, {
-    "start_command": c("yarn_{0}_start_cmd".format(type)),
-    "sync": True,
-  })
-
-def _stop_yarn(deployer, unique_id, type="nm"):
-  logger.debug("Stopping YARN.")
-  deployer.stop(unique_id, {
-    "stop_command": c("yarn_{0}_stop_cmd".format(type)),
-  })
-  logger.debug("Uninstalling YARN.")
-  deployer.uninstall(unique_id)
 
 def setup_suite():
-  global ssh_deployer
+  global deployers
   CWD = os.path.dirname(os.path.abspath(__file__))
-  logger.info("Current working directory: {0}".format(CWD))
-
-  logger.debug("Creating a generic SSH deployer that can be used to deploy ZooKeeper/YARN/Kafka.")
-  ssh_deployer = adhoc_deployer.SSHDeployer("deployer", {
-    'extract': True
-  })
-  runtime.set_deployer("deployer", ssh_deployer)
+  logger.info('Current working directory: {0}'.format(CWD))
 
   _download_packages()
-  _install_yarn(ssh_deployer, "yarn_rm_instance_0")
-  _install_yarn(ssh_deployer, "yarn_nm_instance_0")
-  _start_yarn_rm(ssh_deployer, "yarn_rm_instance_0")
-  _start_yarn_nm(ssh_deployer, "yarn_nm_instance_0")
 
-  #############################
-  # Install Kafka / Zookeeper #
-  #############################
-  kafka_hostname = runtime.get_active_config("kafka_hostname")
-  global kafka_deployer
-  kafka_deployer = kd.KafkaDeployer("kafka", {
-    "pid_keyword": "kafka_job_0",
-    "hostname": kafka_hostname
-  })
-  logger.info("Installing Kafka")
-  zookeeper_executable = runtime.get_active_config("kafka_zookeeper_executable")
-  kafka_executable = runtime.get_active_config("kafka_executable")
-  kafka_deployer.install("kafka_instance_0", {
-    "install_path": runtime.get_active_config("kafka_install_path"),
-    "install_commands": ["install {0} {1}".format(zookeeper_executable, kafka_executable)],
-    "control_script": runtime.get_active_config("kafka_control_script")
-  })
-  runtime.set_deployer("kafka", kafka_deployer)
+  deployers = {
+    'zookeeper': _new_deployer('zookeeper'),
+    'yarn_rm': _new_deployer('yarn_rm'),
+    'yarn_nm': _new_deployer('yarn_nm'),
+    'kafka': _new_deployer('kafka'),
+  }
+
+  # enforce install order through list
+  for name in ['zookeeper', 'yarn_rm', 'yarn_nm', 'kafka']:
+    deployer = deployers[name]
+    runtime.set_deployer(name, deployer)
+    for instance, host in c(name + '_hosts').iteritems():
+      logger.info('Installing {0} on host: {1}.'.format(instance, host))
+      deployer.install(instance, {
+        'hostname': host
+      })
+      deployer.start(instance)
+
   #####################
   # Install Samza Job #
   #####################
-  yarn_process = ""#TODO yarn_deployer.get_process("yarn_instance_0")
+  yarn_process = ''#TODO yarn_deployer.get_process('yarn_instance_0')
   global samza_deployer
-  samza_deployer = syd.SamzaOnYarnDeployer("samza", {
-    "pid_keyword": "samza_job_0",
-    "yarn_host": yarn_process.hostname,
-    "yarn_port": yarn_process.port,
-    "yarn_home": yarn_process.get_yarn_home()
+  samza_deployer = syd.SamzaOnYarnDeployer('samza', {
+    'pid_keyword': 'samza_job_0',
+    'yarn_host': yarn_process.hostname,
+    'yarn_port': yarn_process.port,
+    'yarn_home': yarn_process.get_yarn_home()
   })
-  logger.info("Installing Samza Job")
-  samza_deployer.install("samza_instance_0", {
-    "hostname": runtime.get_active_config("samza_hostname"),
-    "executable": runtime.get_active_config("samza_executable"),
-    "install_path": runtime.get_active_config("samza_install_path")
+  logger.info('Installing Samza Job')
+  samza_deployer.install('samza_instance_0', {
+    'hostname': runtime.get_active_config('samza_hostname'),
+    'executable': runtime.get_active_config('samza_executable'),
+    'install_path': runtime.get_active_config('samza_install_path')
   })
-  runtime.set_deployer("samza", samza_deployer)
-  ###############
-  # Start Kafka #
-  ###############
-  logger.info("Starting Kafka")
-  kafka_deployer.start("kafka_instance_0", {
-    "start_command": "start"
-  })
+  runtime.set_deployer('samza', samza_deployer)
 
   ###################
   # Start Samza Job #
   ###################
-  logger.info("Starting Samza Job")
+  logger.info('Starting Samza Job')
   properties = []
-  for prop_key,prop_value in runtime.get_active_config("samza_properties").iteritems():
+  for prop_key,prop_value in runtime.get_active_config('samza_properties').iteritems():
     properties.append((prop_key, prop_value))
-  samza_deployer.start("samza_instance_0", {
-    "config-factory": runtime.get_active_config("samza_config-factory"),
-    "config-file": runtime.get_active_config("samza_config-file"),
-    "properties": properties
+  samza_deployer.start('samza_instance_0', {
+    'config-factory': runtime.get_active_config('samza_config-factory'),
+    'config-file': runtime.get_active_config('samza_config-file'),
+    'properties': properties
   })
-  logger.info("Ready to run tests")
+  logger.info('Ready to run tests')
 
+# TODO do we need these?
 def setup():
   pass
 
 def teardown():
   pass
 
-def teardown_suite():
-  logger.info("Stopping Samza Job")
-  samza_deployer.stop("samza_instance_0", {})
-  samza_deployer.uninstall("samza_instance_0", {})
+def teardown_suite(): 
+  # TODO properly teardown
+  return
+  for name, deployer in deployers.iteritems():
+    unique_id = name + '_instance_0'
+    deployer.stop(unique_id)
+    deployer.uninstall(unique_id, {
+      'additional_directories': ['/tmp/kafka-logs', '/tmp/zookeeper']
+    })
 
-  logger.info("Stopping Kafka")
-  kafka_deployer.stop("kafka_instance_0", {
-    'stop_command': 'stop'
-  })
-  kafka_process = kafka_deployer.get_process("kafka_instance_0")
-  kafka_deployer.uninstall("kafka_instance_0", {
-    'additional_directories': [kafka_process.zookeeper_home, "/tmp/kafka-logs", "/tmp/zookeeper"]
-  })
-
-  _stop_yarn(ssh_deployer, "yarn_rm_instance_0", "rm")
-  _stop_yarn(ssh_deployer, "yarn_nm_instance_0", "nm")
